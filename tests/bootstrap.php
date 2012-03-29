612257625e33781ab60c86430e5c488a39bf3953
Eriksen Costa <eriksencosta@gmail.com>
2012-03-29 01:39:42 -0300

    removed Twig from lib path, added as composer dependency

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,5 +1,10 @@
 <?php
 
+$file = __DIR__.'/../vendor/.composer/autoload.php';
+if (!file_exists($file)) {
+    throw new RuntimeException('Install dependencies to run test suite.');
+}
+
 spl_autoload_register(function($class) {
     if (0 === strpos($class, 'PassVault_')) {
         $path = __DIR__.'/../lib/'.implode('/', explode('_', $class)).'.php';
